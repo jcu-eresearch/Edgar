1e8794baaf1d5954206690cb911e787fa5a37e4d
Tom Dalling <tom.dalling@gmail.com>
2012-04-11 16:37:14 +1000

    Changed some cake settings to make it stop complaining

--- webapplication/app/Config/core.php
+++ webapplication/app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '1d05287d605b825f0df65de5a11096ca');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '1ae4945be60b25bdee76e4a08e9e0d30');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
@@ -229,7 +229,7 @@
  * Uncomment this line and correct your server timezone to fix 
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+	date_default_timezone_set('Australia/Brisbane');
 
 /**
  * Pick the caching engine to use.  If APC is enabled use it.
