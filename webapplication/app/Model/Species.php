61813ffe8f4796029f8e318a910f2fc2ff1954f5
Robert Pyke <robert.j.pyke@gmail.com>
2012-04-15 17:19:37 +1000

    Added Costa Rica Branch (costa_rica). Added geolocations clustering code from AP03 research.

--- webapplication/app/Model/Species.php
+++ webapplication/app/Model/Species.php
@@ -6,6 +6,21 @@ class Species extends AppModel {
 
     // Define behaviours
     public $actsAs = array(
-        'HPCQueueable' => array()
+        'HPCQueueable' => array(),
+        'Geolocations' => array()
     );
+
+    // A Species has many occurrences.
+    public $hasMany = array(
+        'Occurrence' => array(
+            'className' => 'Occurrence',
+            'dependent' => true,
+        )
+    );
+
+    // Return an array of locations
+    // Needed for the Geolocations behaviour
+    public function getLocationsArray() {
+        return $this->data['Occurrence'];
+    }
 }
