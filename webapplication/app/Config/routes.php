1568efdf30bcca70782c9207315346e39b90843d
Robert Pyke <robert.j.pyke@gmail.com>
2012-04-17 13:51:11 +1000

    Fixed bad route in routes file for home

--- webapplication/app/Config/routes.php
+++ webapplication/app/Config/routes.php
@@ -25,7 +25,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'species', 'action' => 'map', 'home'));
+	Router::connect('/', array('controller' => 'species', 'action' => 'map'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
