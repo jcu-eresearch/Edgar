00748b0cac970e21d12be6021d979a412f005f78
Tom Dalling <tom.dalling@gmail.com>
2012-04-18 20:01:24 +1000

    Cron job stuff for importer
    I've set up a cron job at 1am every day on tdh-tools-2

---
+++ importing/bin/ala_cron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add to cron like so (runs every day at 1am):
+# min hr  dom mth dow cmd
+# 0   1   *   *   *   /home/jc171154/Edgar/importing/bin/ala_cron.sh
+
+IMPORTER_DIR="/home/jc171154/Edgar/importing"
+LOG_DIR="$IMPORTER_DIR/logs"
+SYNC_SCRIPT="$IMPORTER_DIR/bin/ala_db_update.py"
+CONFIG_FILE="$IMPORTER_DIR/config.json"
+
+LOG_FILE="$LOG_DIR/$(date "+%Y-%m-%d %H:%M:%S").log"
+"$SYNC_SCRIPT" "$CONFIG_FILE" &> "$LOG_FILE"
