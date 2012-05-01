625085e01474a1b8386946cb658963336a66bf2b
Robert Pyke <robert.j.pyke@gmail.com>
2012-05-01 20:52:02 +1000

    Updated species_map.js to use species_route_url for generation of autocomplete.json url.

--- webapplication/app/webroot/js/species_map.js
+++ webapplication/app/webroot/js/species_map.js
@@ -374,7 +374,7 @@ $(document).ready(function() {
 
     $('#species_autocomplete').autocomplete({
         minLength: 2,
-        source: '/species/autocomplete.json',
+        source: species_route_url + '/autocomplete.json',
         select: function(event, ui) {
             changeSpecies(ui.item.id, ui.item.value);
         }
