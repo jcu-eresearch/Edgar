d0170be5dbce295ec29ea69aefd38dcad3c2fd86
Robert Pyke <robert.j.pyke@gmail.com>
2012-06-01 16:55:03 +1000

    Added code to auto-magically determine the threshold.

--- mapping/map_with_auto_determine_threshold.php
+++ mapping/map_with_auto_determine_threshold.php
@@ -12,10 +12,50 @@
     $data = null;
     $data = $_GET['DATA'];
 
-    // $shapepath = $map->get('shapepath');
 
     $layer = $map->getLayerByName('DISTRIBUTION');
     $threshold = $_GET['THRESHOLD'];
+
+    if(is_numeric($threshold == false)) {
+        $threshold = "0";
+    }
+
+    try {
+        $shapepath = $map->shapepath;
+        $datapath  = $shapepath."/".$data; 
+        $data_dir  = dirname($shapepath."/".$data); 
+        $maxent_results_path = $data_dir."/"."maxentResults.csv";
+
+        $row = 1;
+        $threshold_key = "Equate entropy of thresholded and original distributions logistic threshold";
+        $threshold_key_pos = 0;
+        $det_threshold = $threshold;
+        if (($handle = fopen($maxent_results_path, "r")) !== FALSE) {
+            while (($csv_data = fgetcsv($handle, 10000, ",")) !== FALSE) {
+                $num = count($csv_data);
+                if ($row === 1) {
+                    for ($c=0; $c < $num; $c++) {
+                        if ($csv_data[$c] == $threshold_key) {
+                            $threshold_key_pos = $c;
+                        }
+                    }
+                } elseif($row === 2) {
+                    $det_threshold = $csv_data[$threshold_key_pos];
+                }
+                $row++;
+            }
+            fclose($handle);
+        }
+
+        if (is_numeric($det_threshold)) {
+            $threshold = $det_threshold;
+        }
+
+    } catch (Exception $e) {
+        // Don't do anything.
+        // We only updated the threshold in the case where everything else worked.
+    }
+
 
     // Pull out every class in the map file.
     while($layer->removeClass(0) != NULL);
