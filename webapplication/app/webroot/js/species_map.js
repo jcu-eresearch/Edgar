30d95dfee1d254d5a26ef6e534d05a9cc149aa91
Robert Pyke <robert.j.pyke@gmail.com>
2012-07-09 14:18:03 +1000

    Removed alert, changed to log.error

--- webapplication/app/webroot/js/species_map.js
+++ webapplication/app/webroot/js/species_map.js
@@ -450,9 +450,11 @@ function updateWindowHistory() {
 }
 
 function handleBlankTile() {
-    alert("We don't have the map for that species :(");
+    console.error("Failed to load tile.");
+    //    alert("We don't have the map for that species :(");
 
-//    this.src = "";
+
+    //    this.src = "";
     this.src = Edgar.baseUrl + "img/blank.png";
 }
 
