213e6c5f3669be41620c388811bbc4e48540c7f1
Tom Dalling <tom.dalling@gmail.com>
2012-07-18 14:51:02 +1000

    Updated num_dirty_occurrences after vetting

--- importing/edgar_importing/vet.py
+++ importing/edgar_importing/vet.py
@@ -60,11 +60,25 @@ def vet_species(args, connection):
             len(vettings))
 
     rows_remaining = query.rowcount
+    rows_changed = 0
     for occrow in query:
-        update_occurrence(occrow, vettings, connection)
+        if update_occurrence(occrow, vettings, connection):
+            rows_changed += 1
+
         rows_remaining -= 1
         if rows_remaining % 10000 == 0:
             log_info('%d rows remaining', rows_remaining)
+
+    log_info('Dirtied %d rows in total', rows_changed)
+    if rows_changed > 0:
+        connection.execute('''
+            UPDATE species
+            SET num_dirty_occurrences = num_dirty_occurrences + {n}
+            WHERE id = {sid};
+            '''.format(
+                n=rows_changed,
+                sid=species['id']
+            ))
 
 
 def update_occurrence(occrow, ordered_vettings, connection):
@@ -101,6 +115,9 @@ def update_occurrence(occrow, ordered_vettings, connection):
                 classi=classification,
                 occid=occrow['id']
             ))
+        return True
+    else:
+        return False
 
 
 def ordered_vettings_for_species_id(species_id, connection):
