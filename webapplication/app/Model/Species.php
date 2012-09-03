82e1b0693b7a3d9c6e98cd3d3e98b8af18669788
Robert Pyke <robert.j.pyke@gmail.com>
2012-09-04 09:12:50 +1000

    Added some comments.

--- webapplication/app/Model/Species.php
+++ webapplication/app/Model/Species.php
@@ -3,15 +3,15 @@
 // File: app/Model/Species.rb
 class Species extends AppModel {
     public $name = 'Species';
-    public $recursive = -1; //stops queries fetching all the occurrences
+    public $recursive = -1; // stops queries fetching all the occurrences
 
-    // Define behaviours
+    // Define behaviours this model exhibits
     public $actsAs = array(
         'HPCQueueable' => array(),
         'Geolocations' => array()
     );
 
-    // A Species has many occurrences.
+    // A Species has many occurrences and vettings (dependent destory)
     public $hasMany = array(
         'Occurrence' => array(
             'className' => 'Occurrence',
@@ -58,6 +58,9 @@ class Species extends AppModel {
         );
     }
 
+    // Marks an arbritrary species as needing vetting
+    //
+    // TODO -> consider updating to work from $this->data['Species']['id']
     public function markAsNeedingVetting($species_id){
         $this->getDataSource()->execute(
             'UPDATE species SET needs_vetting_since = NOW() '.
@@ -67,7 +70,7 @@ class Species extends AppModel {
         );
     }
 
-    // Get the canonical name of the species
+    // Returns the canonical name of the species
     public function canonicalName() {
         $longName = $this->data['Species']['common_name'] . " (" . $this->data['Species']['scientific_name'] . ")";
         $cleanName = preg_replace("[^A-Za-z0-9'-_., ()]", "_", $longName);
@@ -75,8 +78,12 @@ class Species extends AppModel {
     }
 
     // Returns a PDOStatement of clustered occurrence rows within the bounding box for this species
-    // if $lat_lng_round_to_nearest_nth_fraction is null, don't round (simply group by).
+    // if $lat_lng_round_to_nearest_nth_fraction is null, don't round (simply group by)
+    // limit and offset are only applied if both are set (not null)
+    // Note: This function always groups by position (i.e. you can't get individual occurrences from this function)
     public function detailedClusteredOccurrencesInBounds($bounds, $lat_lng_round_to_nearest_nth_fraction = null, $offset=0, $limit=null) {
+
+        // use postgis to set the bounding box
         $bounds = sprintf("SetSRID('BOX(%.12F %.12F,%.12F %.12F)'::box2d,4326)",
                           $bounds['min_longitude'],
                           $bounds['min_latitude'],
@@ -86,17 +93,21 @@ class Species extends AppModel {
         $query_args = array();
         $sql = "";
 
+        // if not rounding to nearest fraction...
         if ( is_null($lat_lng_round_to_nearest_nth_fraction) ) {
             $sql = $sql .
             "SELECT ".
               "CAST (ST_X(location) as numeric) as longitude, ".
               "CAST (ST_Y(location) as numeric) as latitude, ";
+
+        // else - rounding to nearest nth of a fraction, so do some rounding maths on the lat/lng
         } else {
             $sql = $sql .
             "SELECT ".
               "( floor(CAST (ST_X(location) as numeric) * ?) / ? ) + (1.0 / ( 2.0 * ? ) ) as longitude, ".
               "( floor(CAST (ST_Y(location) as numeric) * ?) / ? ) + (1.0 / ( 2.0 * ? ) ) as latitude, ";
 
+            // set appropriate query args
             $query_args[] = $lat_lng_round_to_nearest_nth_fraction;
             $query_args[] = $lat_lng_round_to_nearest_nth_fraction;
             $query_args[] = $lat_lng_round_to_nearest_nth_fraction;
@@ -104,6 +115,8 @@ class Species extends AppModel {
             $query_args[] = $lat_lng_round_to_nearest_nth_fraction;
             $query_args[] = $lat_lng_round_to_nearest_nth_fraction;
         }
+
+        // get sums of classification counts
         $sql = $sql .
               "sum(case when classification = 'unknown' then 1 else 0 end) as unknown_count, ".
               "sum(case when classification = 'invalid' then 1 else 0 end) as invalid_count, ".
@@ -131,6 +144,8 @@ class Species extends AppModel {
             'GROUP BY longitude, latitude ';
         $query_args[] = $this->data['Species']['id'];
 
+        // if both limit and offset are set,
+        // then apply limit/offset and order by position
         if ( !is_null($limit) && !is_null($offset) ) {
           $sql = $sql .
             'LIMIT ? ' .
@@ -141,6 +156,7 @@ class Species extends AppModel {
           $query_args[] = $offset;
         }
 
+        // execute the sql query we've been building
         return $this->getDataSource()->execute(
             $sql,
             array(),
