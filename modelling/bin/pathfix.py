ee33f7e4855b58232ee5a6303c26baf473a17c21
Robert Pyke <robert.j.pyke@gmail.com>
2012-09-24 08:53:42 +1000

    Updated pathfix to include more paths.

--- modelling/bin/pathfix.py
+++ modelling/bin/pathfix.py
@@ -8,3 +8,13 @@ sys.path.append(modellingsrcpath)
 
 importingsrcpath = os.path.realpath(os.path.dirname(__file__) + '/../../importing/edgar_importing')
 sys.path.append(importingsrcpath)
+
+for root, dirs, files in os.walk(os.path.realpath(os.path.dirname(__file__) + '/../../importing')):
+    for f in files:
+        fullpath = os.path.join(root, f)
+	sys.path.append(fullpath)
+
+for root, dirs, files in os.walk(os.path.realpath('/usr/lib/python2.6/site-packages/')):
+    for f in files:
+        fullpath = os.path.join(root, f)
+	sys.path.append(fullpath)
